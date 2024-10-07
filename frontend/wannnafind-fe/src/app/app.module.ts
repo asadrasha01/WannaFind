489703f9ca28b808d10f05f39db4c96986b3fdb2
Asadbek Rashidov <arashidov2001@gmail.com>
2024-10-07 15:40:41 +0300

    Replaced frontend folder

---
+++ frontend/wannnafind-fe/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ListingPageComponent } from './pages/home-page/listing-page/listing-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    ListingPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
