4f5ccecb6cab44379420f639a45f4689d8fd6d7d
Asadbek Rashidov <arashidov2001@gmail.com>
2024-10-07 17:23:37 +0300

    Added header and footer

--- frontend/wannnafind-fe/src/app/app.module.ts
+++ frontend/wannnafind-fe/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ListingPageComponent } from './pages/home-page/listing-page/listing-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    ListingPageComponent
+    ListingPageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
