d07cc9d52296ec241943b693c6bdc658389d9b34
Asadbek Rashidov <arashidov2001@gmail.com>
2024-10-11 14:12:10 +0300

    added home page

--- frontend/wannnafind-fe/frontend/wannnafind-fe/src/app/app-routing.module.ts
+++ frontend/wannnafind-fe/frontend/wannnafind-fe/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
-// frontend/wannnafind-fe/src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent }, // Default route
-  { path: 'home', component: HomePageComponent } // Optional: route for '/home'
+  // {path: 'home', component: HomePageComponent }
 ];
 
 @NgModule({
